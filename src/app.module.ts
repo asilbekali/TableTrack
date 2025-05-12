2c44978cb3ae731cee330ceb90204442f388cf94
asilbekali <asilbekabdugafforov7@gmail.com>
2025-05-12 18:21:34 +0500

    add order curd but not finished

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { CategoryModule } from './category/category.module';
 import { ProductModule } from './product/product.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { ProductModule } from './product/product.module';
     AuthModule,
     CategoryModule,
     ProductModule,
+    OrderModule,
   ],
   controllers: [AppController],
   providers: [AppService],
